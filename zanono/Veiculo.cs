dd598d026963d4f93b436d0291a7ed751fd0308c
curso.ads <curso.ads@LAB06-11>
2022-08-15 22:02:58 -0300

    Adicionar arquivos de projeto.

---
+++ zanono/Veiculo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zanono
+{
+    public class Veiculo
+    {
+        // declaração de atributos
+        string marca;
+        string modelo;
+        string placa;
+        int ano;
+        // método para atributos valores
+        public void atribuir(string ma,
+            string mo, string pl, int a)
+        {
+            this.ano = a;
+            this.marca = ma;
+            this.modelo = mo;
+            this.placa = pl;
+        }
+        public string retorna()
+        {
+            return "Modelo=" + this.marca +
+                 "\nModelo=" + this.modelo +
+                 "\nAno de fabricação=" + this.ano
+                 + "\nPlaca=" + this.placa;
+        } 
+    }
+}
